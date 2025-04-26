a28a5cfb46244b05aec593cf12e42d218b7c3b60
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-04-26 11:56:14 +0900

    3-3-1.データを表示させよう

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,114 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷オフィスを移転しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界リーターTOP30に選出されました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+  ],
+};
+
 export default function Home() {
   //JavaScriptの定数や式などを書くことができる
   // const name = "世界";
 
   // HTMLのようなマークアップを書くことができる
   // JavaScriptを書いたり、参照する際には{}で囲む
+
+  // 表示件数の絞り込み
+  const slicceData = data.contents.slice(0, 2);
+
   return (
-    <section className={styles.top}>
-      <div>
-        {/* CSS moduleをクラスに読み込む */}
-        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテクニックカンパニーです。
-        </p>
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-    </section>
+    // フラグメント
+    <>
+      <section className={styles.top}>
+        <div>
+          {/* CSS moduleをクラスに読み込む */}
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテクニックカンパニーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <ul>
+          {/* mapメソッドで配列要素を繰り返し表示 */}
+          {slicceData.map((article) => (
+            <li key={article.id} className={styles.list}>
+              <div className={styles.link}>
+                <Image
+                  className={styles.image}
+                  src="/no-image.png"
+                  alt="No Image"
+                  width={1200}
+                  height={630}
+                />
+                <dl className={styles.content}>
+                  <dt className={styles.newsItemTitle}>{article.title}</dt>
+                  <dd className={styles.meta}>
+                    <span className={styles.tag}>{article.category.name}</span>
+                    <span className={styles.date}>
+                      <Image
+                        src="/clock.svg"
+                        alt=""
+                        width={16}
+                        height={16}
+                        priority
+                      />
+                      {article.publishedAt}
+                    </span>
+                  </dd>
+                </dl>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
