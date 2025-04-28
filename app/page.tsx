a976ef610705b54eda9f43c3decdaa88fd1269cb
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-04-28 14:49:38 +0900

    3-3-2.コンポーネントを作ろう

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+// Next.jsでは@を使うことで絶対パスでファイルを参照
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
 
 const data: {
   contents: News[];
@@ -53,7 +48,8 @@ export default function Home() {
   // JavaScriptを書いたり、参照する際には{}で囲む
 
   // 表示件数の絞り込み
-  const slicceData = data.contents.slice(0, 2);
+  const sliceData = data.contents.slice(0, 2);
+  // const sliceData: News = [];
 
   return (
     // フラグメント
@@ -76,38 +72,11 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <ul>
-          {/* mapメソッドで配列要素を繰り返し表示 */}
-          {slicceData.map((article) => (
-            <li key={article.id} className={styles.list}>
-              <div className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src="/no-image.png"
-                  alt="No Image"
-                  width={1200}
-                  height={630}
-                />
-                <dl className={styles.content}>
-                  <dt className={styles.newsItemTitle}>{article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>{article.category.name}</span>
-                    <span className={styles.date}>
-                      <Image
-                        src="/clock.svg"
-                        alt=""
-                        width={16}
-                        height={16}
-                        priority
-                      />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={sliceData} />
+        {/* レイアウト・余白はdivでスタイル */}
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
       </section>
     </>
   );
