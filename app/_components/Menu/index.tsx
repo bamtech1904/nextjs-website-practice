3c28be13eabee2cd72212924eb09cf5b865feac9
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-04-29 21:58:36 +0900

    4-4-3.useStateでWebサイトに動きをつけよう

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -2,16 +2,22 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import cx from "classnames";
 
 import styles from "./index.module.css";
 
 export default function Menu() {
-  const open = () => {
-    document.querySelector("nav")?.classList.add(styles.open);
-  };
+  // useState(isOpen:状態の変数、setOpen:セッター)
+  const [isOpen, setOpen] = useState<boolean>(false);
+  // isOpenに状態を設定して保存
+  const open = () => setOpen(true);
+  const close = () => setOpen(false);
+
   return (
     <div>
-      <nav className={styles.nav}>
+      {/* isOpenがtrueの場合にopenクラスを適用 */}
+      <nav className={cx(styles.nav, isOpen && styles.open)}>
         <ul className={styles.items}>
           <li>
             <Link href="/news">ニュース</Link>
@@ -23,6 +29,16 @@ export default function Menu() {
             <Link href="/contact">お問い合わせ</Link>
           </li>
         </ul>
+        {/* クリックするとclose関数が実行される */}
+        <button className={cx(styles.button, styles.close)} onClick={close}>
+          <Image
+            src="/close.svg"
+            alt="閉じる"
+            width={24}
+            height={24}
+            priority
+          />
+        </button>
       </nav>
       <button className={styles.button} onClick={open}>
         <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
