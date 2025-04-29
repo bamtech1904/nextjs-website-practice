c8d3e7ff5861181e7547619a8a54fe63b718fd9a
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-04-30 08:33:20 +0900

    5-3-1.事前準備をしよう

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,17 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
 export type Category = {
   name: string;
 };
@@ -10,4 +24,27 @@ export type News = {
   };
   publishedAt: string;
   createdAt: string;
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+// クライアントの作成
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// メンバーの一覧を表示
+export const getMemberList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
