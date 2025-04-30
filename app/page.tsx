41aaa4671bdf40d20131cc452cbc6ba11eb903c5
bamtech <96427750+bamtech1904@users.noreply.github.com>
2025-04-30 22:05:49 +0900

    5-4-3.ソースコードを書き換えよう

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 // Next.jsでは@を使うことで絶対パスでファイルを参照
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
 
-const data: {
-  contents: News[];
-} = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷オフィスを移転しました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "2",
-      title: "当社CEOが業界リーターTOP30に選出されました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "3",
-      title: "テストの記事です",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/04/19",
-      createdAt: "2023/04/19",
-    },
-  ],
-};
-
-export default function Home() {
+export default async function Home() {
   //JavaScriptの定数や式などを書くことができる
   // const name = "世界";
 
@@ -48,8 +15,11 @@ export default function Home() {
   // JavaScriptを書いたり、参照する際には{}で囲む
 
   // 表示件数の絞り込み
-  const sliceData = data.contents.slice(0, 2);
+  // const sliceData = data.contents.slice(0, 2);
   // const sliceData: News = [];
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
 
   return (
     // フラグメント
@@ -72,7 +42,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={sliceData} />
+        <NewsList news={data.contents} />
         {/* レイアウト・余白はdivでスタイル */}
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
